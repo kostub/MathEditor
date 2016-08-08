a5a87f3bb5da8556e9b66eaf8af3af8c7c1794bc
jdolecki <jakub.dolecki2@gmail.com>
2016-08-08 17:00:26 -0400

    Actually fixes bug with highlight color

--- mathEditor/keyboard/MTKeyboardKeyButton.m
+++ mathEditor/keyboard/MTKeyboardKeyButton.m
@@ -8,17 +8,16 @@
 
 #import "MTKeyboardKeyButton.h"
 
-@implementation MTKeyboardKeyButton
-
-- (UIColor*) defaultColor {
-    static UIColor *defaultColor = nil;
-    
-    if (defaultColor == nil) {
-        defaultColor = self.backgroundColor;
-    }
-    
-    return defaultColor;
+@implementation MTKeyboardKeyButton {
+    UIColor *_originalBackgroundColor;
 }
+
+- (void) didMoveToSuperview {
+    [super didMoveToSuperview];
+    
+    _originalBackgroundColor = self.backgroundColor;
+}
+
 
 /** Overwrites UIButton's setHighlighted to add a highlight color when user highlights the button
  */
@@ -26,7 +25,7 @@
     
     // Grayish color for when button is highlighted
     UIColor *kMTKeyboardKeyButtonHighlightedColor = [UIColor colorWithRed:0.8f green:0.8f blue:0.8f alpha:1.f];
-    UIColor *kMTKeyboardKeyButtonDefaultColor = [self defaultColor];
+    UIColor *kMTKeyboardKeyButtonDefaultColor = _originalBackgroundColor;
     
     [super setHighlighted:highlighted];
     
