d01fb627511355d88b6d2d17b6217a4918fc2e7c
Christian Beer <christian.beer@chbeer.de>
2022-08-29 17:36:24 +0200

    Moved to tag for iosMath

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/chbeer/iosMath.git", branch: "master"),
+        .package(url: "https://github.com/chbeer/iosMath.git", from: "0.9.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
